690d53d0a1c27fb2bc370cee48f6b1aeeace32fb
Matt Richards <matt@lucidmoon.co.uk>
2012-08-16 17:20:22 +0200

    Second pass.

--- 4test.js
+++ 4test.js
@@ -1,198 +1,197 @@
 (function() {
 
-var table = {
+    var table = {
 
-    cells: [
-        /* Row 1 */
-        [
-        {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }}
-    ],
-        /* Row 2 */
-        [
-        {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 2,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }}
-    ],
-        /* Row 3 */
-        [
-        {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 1,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }},
-    {
-        rowspan: 1,
-        colspan: 2,
-        border: {
-            top: 'solid',
-            right: 'solid',
-            bottom: 'solid',
-            left: 'solid'
-        }}
-    ]
-        ]
-
-};
-
-
-
-/**
- * Normalise a table's border properties
- * @param {Object} table - The table to be normalised
- * @return {Object} table - The normalised table
- */
-var normaliseTableBorders = function(table) {
-    var row, cell, validBorderStyles;
-
-    validBorderStyles = [
-        ['strong','solid'],
-        ['normal','dashed'],
-        ['weak','dotted'],
-        ['none']
-    ];
-
-    var normaliseBorderValue = function(borderStyleValue) {
-        for (style in validBorderStyles) {
-            if (validBorderStyles[style].indexOf(borderStyleValue) > 0) {
-                return validBorderStyles[style][0];
+        cells: [ /* Row 1 */ [{
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'dashed',
+                right: 'dashed',
+                bottom: 'dashed',
+                left: 'dashed'
             }
-        }
-        return false;
+        }, {
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'dashed'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }], /* Row 2 */ [{
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'dashed',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 2,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }], /* Row 3 */ [{
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 1,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }, {
+            rowspan: 1,
+            colspan: 2,
+            border: {
+                top: 'solid',
+                right: 'solid',
+                bottom: 'solid',
+                left: 'solid'
+            }
+        }]]
+
     };
 
-    var normaliseBorderValues = function(target, source) {
-        var style, tmp, match, returnValue = '';
 
-        target = normaliseBorderValue(target);
-        source = normaliseBorderValue(source);
 
-        if (target === source) {
-            return target;
-        } else {
-            throw new Error("Incompatible types");
-        }
-        
-    };
-    
-    for (var _row = 0; _row < table.cells.length; _row++) {
-        row = table.cells[_row];
-        for (var _col = 0; _col < row.length; _col++) {
-            cell = row[_col];
-            
-            var siblings = {
-                top: table.cells[_row - 1] ? table.cells[_row - 1][_col] : null,
-                right: table.cells[_row][_col + 1],
-                bottom: table.cells[_row + 1] ? table.cells[_row + 1][_col] : null,
-                left: table.cells[_row][_col - 1]
-            };
+    /**
+     * Normalise a table's border properties
+     * @param {Object} table - The table to be normalised
+     * @return {Object} table - The normalised table
+     */
+    var normaliseTableBorders = function(table) {
+            var row, cell, validBorderStyles;
 
-            for (position in cell.border) {
+            validBorderStyles = [
+                ['strong', 'solid'],
+                ['normal', 'dashed'],
+                ['weak', 'dotted'],
+                ['none']
+            ];
 
-                if (position === 'top' && cell.border[position]) {
-                    cell.border[position] = normaliseBorderValues(cell.border[position],cell.border[position])
-                }
-                if (position === 'right' && cell.border[position]) {
-                    if (siblings.right && siblings.right.border) {
-                        if (siblings.right.border.left) {
-                            siblings.right.border.left = normaliseBorderValues(siblings.right.border.left,cell.border[position])
-                        }
-                        
-                    }   
-                    delete cell.border[position];                
-                }
-                if (position === 'bottom' && cell.border[position]) {
-                    if (siblings.bottom && siblings.bottom.border) {
-                        if (siblings.bottom.border.top) {
-                            siblings.bottom.border.top = normaliseBorderValues(siblings.bottom.border.top,cell.border[position]);
+            /**
+             * Normalise a borders value
+             * @param {String} borderStyleValue - The border value to be normalised
+             * @return {String} normalisedValue - The normalised border value
+             */
+            var normaliseBorderValue = function(borderStyleValue) {
+                    var normalisedValue = false;
+                    for (var _i = 0; _i < validBorderStyles.length; _i = _i + 1) {
+                        if (validBorderStyles[_i].indexOf(borderStyleValue) > 0) {
+                            normalisedValue = validBorderStyles[_i][0];
+                        } else {
+                            var lastUnmatchedIndexId = validBorderStyles[_i].indexOf(borderStyleValue);
                         }
                     }
-                    delete cell.border[position];                
-                }
-                if (position === 'left' && cell.border[position]) {
-                    cell.border[position] = normaliseBorderValues(cell.border[position],cell.border[position])
-                }
 
+                    if (normalisedValue) {
+                        return normalisedValue;
+                    } else {
+                        console.log(lastUnmatchedIndexId);
+                        throw new Error("Invalid border type: " + borderStyleValue);
+                    }
+                };
 
-                
+            /**
+             * Validate (pre-normalised) adjacent borders values
+             * @param {String} borderStyleValue - The border value to be normalised
+             * @return {String} normalisedValue - The normalised border value
+             */
+            var validateBorderValues = function(target, source) {
+                    if (target === source) {
+                        return target;
+                    } else {
+                        throw new Error("Incompatible types: '" + target + "' and '" + source + "'");
+                    }
+                };
+
+            // First Pass.  Simply normalise values or throw an error.
+            for (var _row = 0; _row < table.cells.length; _row++) {
+                row = table.cells[_row];
+                for (var _col = 0; _col < row.length; _col++) {
+                    cell = row[_col];
+                    for (position in cell.border) {
+                        cell.border[position] = normaliseBorderValue(cell.border[position]);
+                    }
+                }
             }
+
+            // Second Pass. Validate & re-apply border values
+            for (var _row = 0; _row < table.cells.length; _row++) {
+                row = table.cells[_row];
+                for (var _col = 0; _col < row.length; _col++) {
+                    cell = row[_col];
+
+                    var siblings = {
+                        top: table.cells[_row - 1] ? table.cells[_row - 1][_col] : null,
+                        right: table.cells[_row][_col + 1],
+                        bottom: table.cells[_row + 1] ? table.cells[_row + 1][_col] : null,
+                        left: table.cells[_row][_col - 1]
+                    };
+
+                    for (position in cell.border) {
+                        if (position === 'right' && cell.border[position]) {
+                            if (siblings.right && siblings.right.border && siblings.right.border.left) {
+                                siblings.right.border.left = validateBorderValues(siblings.right.border.left, cell.border[position])
+                            }
+                            delete cell.border[position];
+                        }
+                        if (position === 'bottom' && cell.border[position]) {
+                            if (siblings.bottom && siblings.bottom.border && siblings.bottom.border.top) {
+                                siblings.bottom.border.top = validateBorderValues(siblings.bottom.border.top, cell.border[position]);
+                            }
+                            delete cell.border[position];
+                        }
+                    }
+                }
+            }
+            return table;
         }
-    }
-    
-    return table;
-}
 
-console.log(normaliseTableBorders(table));
+    console.log(normaliseTableBorders(table));
 
-}())
+}());
