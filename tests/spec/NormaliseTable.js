a18d13cff368e084d2449f2c2a76892ff9a9e06a
Matt Richards <matt@lucidmoon.co.uk>
2012-08-16 23:39:10 +0200

    Update tests

--- tests/spec/NormaliseTable.js
+++ tests/spec/NormaliseTable.js
@@ -27,4 +27,25 @@ describe("NF.normaliseTableBorders()", function() {
     });
   });
   
+  describe("Table 3", function() {
+    beforeEach(function() {
+      this.table.cells[1][1].border.bottom = 'weak';
+      this.table.cells[2][2].border.top = 'strong';
+    })
+    it("Should Throw Incompatible types: 'strong' and 'weak' Error", function() {
+      var self = this;
+      expect(function() {return NF.normaliseTableBorders(self.table)}).toThrow(new Error("Incompatible types: 'strong' and 'weak'"));
+    });  
+  });
+
+  describe("Table 4", function() {
+    beforeEach(function() {
+      this.table.cells[1][1].border.bottom = 'kangaroo';
+    })
+    it("Should Throw Invalid Border Type: 'kangaroo' Error", function() {
+      var self = this;
+      expect(function() {return NF.normaliseTableBorders(self.table)}).toThrow(new Error("Invalid border type: kangaroo"));
+    });  
+  });
+
 }); 
